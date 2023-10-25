4eabf973ab806c17e83afabc261da9c30936481c
Kaius <kaius.loos@gmail.com>
2023-10-25 11:50:52 +0300

    add `Arbitrary` impls for `IndexMultimap`

--- src/arbitrary.rs
+++ src/arbitrary.rs
@@ -1,7 +1,7 @@
 #[cfg(feature = "arbitrary")]
 #[cfg_attr(docsrs, doc(cfg(feature = "arbitrary")))]
 mod impl_arbitrary {
-    use crate::{IndexMap, IndexSet};
+    use crate::{IndexMap, IndexMultimap, IndexSet};
     use arbitrary::{Arbitrary, Result, Unstructured};
     use core::hash::{BuildHasher, Hash};
 
@@ -33,12 +33,27 @@ mod impl_arbitrary {
             u.arbitrary_take_rest_iter()?.collect()
         }
     }
+
+    impl<'a, K, V, S> Arbitrary<'a> for IndexMultimap<K, V, S>
+    where
+        K: Arbitrary<'a> + Hash + Eq,
+        V: Arbitrary<'a>,
+        S: BuildHasher + Default,
+    {
+        fn arbitrary(u: &mut Unstructured<'a>) -> Result<Self> {
+            u.arbitrary_iter()?.collect()
+        }
+
+        fn arbitrary_take_rest(u: Unstructured<'a>) -> Result<Self> {
+            u.arbitrary_take_rest_iter()?.collect()
+        }
+    }
 }
 
 #[cfg(feature = "quickcheck")]
 #[cfg_attr(docsrs, doc(cfg(feature = "quickcheck")))]
 mod impl_quickcheck {
-    use crate::{IndexMap, IndexSet};
+    use crate::{IndexMap, IndexMultimap, IndexSet};
     use alloc::boxed::Box;
     use alloc::vec::Vec;
     use core::hash::{BuildHasher, Hash};
@@ -63,6 +78,22 @@ mod impl_quickcheck {
     impl<T, S> Arbitrary for IndexSet<T, S>
     where
         T: Arbitrary + Hash + Eq,
+        S: BuildHasher + Default + Clone + 'static,
+    {
+        fn arbitrary(g: &mut Gen) -> Self {
+            Self::from_iter(Vec::arbitrary(g))
+        }
+
+        fn shrink(&self) -> Box<dyn Iterator<Item = Self>> {
+            let vec = Vec::from_iter(self.clone());
+            Box::new(vec.shrink().map(Self::from_iter))
+        }
+    }
+
+    impl<K, V, S> Arbitrary for IndexMultimap<K, V, S>
+    where
+        K: Arbitrary + Hash + Eq,
+        V: Arbitrary,
         S: BuildHasher + Default + Clone + 'static,
     {
         fn arbitrary(g: &mut Gen) -> Self {
